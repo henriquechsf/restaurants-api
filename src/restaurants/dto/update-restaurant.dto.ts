3bb4da93391c5ec50339ec1428462d1d6c9b44a6
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-16 18:15:11 -0300

    feat: validation optional on update

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,11 +1,34 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schemas/restaurant.schema';
 
 export class UpdateRestaurantDTO {
+  @IsString()
+  @IsOptional()
   readonly name: string;
+
+  @IsString()
+  @IsOptional()
   readonly description: string;
+
+  @IsEmail({}, { message: 'Please enter correct email' })
+  @IsOptional()
   readonly email: string;
+
+  @IsPhoneNumber('BR')
+  @IsOptional()
   readonly phone: string;
+
+  @IsString()
+  @IsOptional()
   readonly address: string;
+
+  @IsEnum(Category, { message: 'Please enter correct category' })
+  @IsOptional()
   readonly category: Category;
-  readonly images?: object[];
 }
