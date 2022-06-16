bd382eed7e9e25eb67c74ddc570ebeff9718f65c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-16 16:47:20 -0300

    feat: get restaurants

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
 
 @Controller('restaurants')
-export class RestaurantsController {}
+export class RestaurantsController {
+  constructor(private restaurantsService: RestaurantsService) {}
+
+  @Get()
+  async getAllRestaurants(): Promise<Restaurant[]> {
+    return await this.restaurantsService.findAll();
+  }
+}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
 
 @Injectable()
-export class RestaurantsService {}
+export class RestaurantsService {
+  constructor(
+    @InjectModel(Restaurant.name)
+    private restaurantModel: mongoose.Model<Restaurant>,
+  ) {}
+
+  async findAll(): Promise<Restaurant[]> {
+    return await this.restaurantModel.find();
+  }
+}
