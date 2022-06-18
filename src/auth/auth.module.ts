dbb9aea08824219d0a29c6b6fb2e8e18a0134449
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-18 10:08:03 -0300

    config passport and jwt in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserSchema } from './schemas/user.schema';
@@ -12,6 +15,18 @@ import { UserSchema } from './schemas/user.schema';
         schema: UserSchema,
       },
     ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        };
+      },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
